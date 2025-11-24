73e56e452f3930ccbed13f879bd83e8c5391dc58
user <benalleng@gmail.com>
2025-11-24 12:48:24 -0500

    Rename banner to BOSS Challenge

--- shared/Boss.tsx
+++ shared/Boss.tsx
@@ -7,7 +7,7 @@ export default function Boss() {
         <div className="boss-bg-wrapper w-[223px] rounded-[3px] border border-white/50 font-audiowide text-base drop-shadow-[0_0_25px_#93EFFFBF] backdrop-blur-xl">
           <div className="boss-bg top-bg" />
           <div className="relative z-10 flex flex-row gap-[5px] px-[15px] py-3">
-            <span className="font-audiowide">APPLY TO THE BOSS PROGRAM</span>
+            <span className="font-audiowide">APPLY TO THE BOSS CHALLENGE</span>
             <Icon className="h-9 w-9 self-center" icon="arrow" />
           </div>
           <div className="boss-bg bottom-bg" />
